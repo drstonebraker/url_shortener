67f7d558edb98b65e4c3c559dc84b720c6a3d273
drstonebraker <drew.stonebraker@gmail.com>
2017-08-16 14:49:13 -0700

    Finish shortened_url model and migration

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,2 +1,11 @@
 class ShortenedUrl < ApplicationRecord
+
+  validates :short_url, uniqueness: true, presence: true
+  validates :long_url, :submitter_id, presence: true
+
+  belongs_to :submitter,
+    primary_key: :id,
+    foreign_key: :submitter_id,
+    class_name: :User
+    
 end
