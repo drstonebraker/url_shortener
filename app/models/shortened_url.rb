8a4d92a6b5ed8ab3c8eb2036005ebede2567c811
drstonebraker <drew.stonebraker@gmail.com>
2017-08-16 15:03:38 -0700

    Write ShortendUrl factory method

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id           :integer          not null, primary key
+#  long_url     :string           not null
+#  short_url    :string           not null
+#  submitter_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class ShortenedUrl < ApplicationRecord
 
   validates :short_url, uniqueness: true, presence: true
@@ -7,5 +19,21 @@ class ShortenedUrl < ApplicationRecord
     primary_key: :id,
     foreign_key: :submitter_id,
     class_name: :User
-    
+
+
+  def self.random_code
+    code = SecureRandom::urlsafe_base64
+    while ShortenedUrl.exists?(short_url: code)
+      code = SecureRandom::urlsafe_base64
+    end
+    code
+  end
+
+  def self.generate(user, long_url)
+    ShortenedUrl.create!({
+      long_url: long_url,
+      short_url: self.random_code,
+      submitter_id: user.id
+      })
+  end
 end
