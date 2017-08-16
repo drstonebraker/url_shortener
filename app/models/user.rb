c379bf51ae2f9791189c845a0b2b039da822112f
drstonebraker <drew.stonebraker@gmail.com>
2017-08-16 15:17:20 -0700

    Write visits create table migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,10 @@
 
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
-  
+
+  has_many :submitted_urls,
+    primary_key: :id,
+    foreign_key: :submitter_id,
+    class_name: :ShortenedUrl
+
 end
